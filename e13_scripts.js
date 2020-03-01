86b6c1e1c47ca6f9ca0286f50909243759ac97c0
Vestfalls <bogd.prihodko@gmail.com>
2020-03-01 22:33:59 +0300

    init js/css

---
+++ e13_scripts.js
@@ -0,0 +1,8 @@
+
+const ELEM_WRAPPER_HEADER_ID = 'e13-header-wrapper';
+const ELEM_WRAPPER_LIST_ID = 'e13-elem-list';
+const ELEM_INPUT_SEARCH_ID = 'e13-input-search';
+const ELEM_LIST_ITEM_ID_PREFIX = 'e13-list-item';
+
+const CLASS_ELEM_BLUR = 'e13-dd-element-blur';
+const CLASS_ELEM_FOCUS = 'e13-dd-element-focus';
