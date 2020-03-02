c3acfc9d9b041136fa242131b68acfd55782cb63
Vestfalls <bogd.prihodko@gmail.com>
2020-03-03 02:36:11 +0300

    update js

--- e13_scripts.js
+++ e13_scripts.js
@@ -6,30 +6,54 @@ const ELEM_LIST_ITEM_ID_PREFIX = 'e13-list-item';
 const CLASS_ELEM_BLUR = 'e13-dd-element-blur';
 const CLASS_ELEM_FOCUS = 'e13-dd-element-focus';
 
+const LOAD_STATUS_FIRST_INIT = 'load_first_init'
+const LOAD_STATUS_READY_TO_LOAD = 'load_ready_to';
+const LOAD_STATUS_IN_PROGRES = 'load_in_progress';
+const LOAD_STATUS_LOAD_COMPLETE = 'load_complete';
+
+const LOAD_TYPE_FIRST_INIT = 'init_e13'
+const LOAD_TYPE_GET_FROM_LOCAL = 'get_ls';
+const LOAD_TYPE_SET_TO_LOCAL = 'set_ls';
+const LOAD_TYPE_REMOVE_WORK_WINDOW = 'remove_ls';
+
+const PREFIX = 'e13-';
+
 const LOGER_ON = true;
 
-let isLoadInProggress = false;
-let isLoadComplete = false;
+let currentLoadStatus = null;
 
 
-document.addEventListener('DOMContentLoaded', function(){
-    document.addEventListener('keydown', e =>{
-        keyListen(e)
-    })
-}) 
+document.addEventListener('keydown', e =>{
+    logger('test');
+    getInitFunc(e);
+})
 
-function keyListen(e){
+function getInitFunc(e){
     if(e.altKey && e.key === 'q' || e.key === 'й'){
-        logger('start');
+        switch (currentLoadStatus){
+            case LOAD_TYPE_FIRST_INIT:
+                logger('init not complete, try to restart');
+                firstInit();
+                break;
+            case LOAD_STATUS_IN_PROGRES:
+                logger('load in progress...')
+                break;
+            default:
+                initWorkWindow();
+        }
     }
 }
-    
-function startLoad(event){
-    console.log(event);
+
+function initWorkWindow(){
+    logger('init_work_window')
 }
 
 function logger(...args){
     if(LOGER_ON){
-        console.log(args)
+        console.log('e13 ----[',args)
     }
+}
+
+function setStatus(status){
+    currentLoadStatus = status;
 }
